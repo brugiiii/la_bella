163b364706893be4f180a4a218131c2c3d9564cb
Eduard <edyabrug91@gmail.com>
2023-06-28 16:02:05 +0300

    add modal

--- js/openModal.js
+++ js/openModal.js
@@ -2,14 +2,24 @@ const refs = {
   priceBackdrop: document.querySelector(".backdrop.price"),
   catalogBackdrop: document.querySelector(".backdrop.catalog"),
   cooperationBackdrop: document.querySelector(".backdrop.cooperation"),
+  closePriceModalButton: document.querySelector(".price-modal__close-button"),
+  closeCatalogModalButton: document.querySelector(
+    ".catalog-modal__close-button"
+  ),
   showPriceButton: document.querySelector("#price-button"),
-  showCatalogButton: document.querySelector(".catalog-list__button"),
+  showCatalogButtons: document.querySelectorAll(".catalog-list__button"),
   showCooperationButton: document.querySelector(".cooperation__button"),
+  bodyEl: document.querySelector("body"),
 };
 
 refs.showPriceButton.addEventListener("click", showPriceModal);
-refs.showCatalogButton.addEventListener("click", showCataloModal);
+refs.showCatalogButtons.forEach((button) =>
+  button.addEventListener("click", showCataloModal)
+);
 refs.showCooperationButton.addEventListener("click", showCooperationModal);
+
+refs.closePriceModalButton.addEventListener("click", hidePriceModal);
+refs.closeCatalogModalButton.addEventListener("click", hideCatalogModal);
 
 refs.priceBackdrop.addEventListener("click", (e) => {
   if (e.target === e.currentTarget) {
@@ -31,12 +41,14 @@ refs.cooperationBackdrop.addEventListener("click", (e) => {
 
 function showPriceModal() {
   refs.priceBackdrop.classList.remove("is-hidden");
+  disableWindowScroll();
 
   window.addEventListener("keydown", closePriceModalByEsc);
 }
 
 function hidePriceModal() {
   refs.priceBackdrop.classList.add("is-hidden");
+  enableWindowScroll();
 
   window.removeEventListener("keydown", closePriceModalByEsc);
 }
@@ -49,12 +61,14 @@ function closePriceModalByEsc(e) {
 
 function showCataloModal() {
   refs.catalogBackdrop.classList.remove("is-hidden");
+  disableWindowScroll();
 
   window.addEventListener("keydown", closeCatalogModalByEsc);
 }
 
 function hideCatalogModal() {
   refs.catalogBackdrop.classList.add("is-hidden");
+  enableWindowScroll();
 
   window.removeEventListener("keydown", closeCatalogModalByEsc);
 }
@@ -67,12 +81,14 @@ function closeCatalogModalByEsc(e) {
 
 function showCooperationModal() {
   refs.cooperationBackdrop.classList.remove("is-hidden");
+  disableWindowScroll();
 
-  window.addEventListener("click", closeCooperationModalByEsc);
+  window.addEventListener("keydown", closeCooperationModalByEsc);
 }
 
 function hideCooperationModal() {
-  refs.cooperationBackdrop.add("is-hidden");
+  refs.cooperationBackdrop.classList.add("is-hidden");
+  enableWindowScroll();
 
   window.removeEventListener("keydown", closeCooperationModalByEsc);
 }
@@ -81,4 +97,12 @@ function closeCooperationModalByEsc(e) {
   if (e.code === "Escape") {
     hideCooperationModal();
   }
+}
+
+function disableWindowScroll() {
+  refs.bodyEl.style.overflow = "hidden";
+}
+
+function enableWindowScroll() {
+  refs.bodyEl.style.overflow = "scroll";
 }
